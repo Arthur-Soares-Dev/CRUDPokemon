6b86f5a12072de47237600d9b16197765ba6350e
Lucas <143721146+lucaswilsondev@users.noreply.github.com>
2024-06-10 16:13:12 -0300

    update forms

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -1,94 +1,97 @@
 @extends('layouts.app')
 @section('conteudo')
 
-<div class="formContainer">
-    <h1>
-        <!-- Pegá-los eu tentarei! -->
-        Salvar Pokemon
-    </h1>
-    <form class="form" action="/store" method="post">
-        @csrf
-        <!--<p>-->
-            <label class="labelForm" for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" value="">
-        <!--</p>
-        <p>-->
-            <label class="labelForm" for="tipo">Tipo:</label>
-            <select class="select" id="tipo" name="tipo">
-                <option value="normal">Normal</option>
-                <option value="fogo">Fogo</option>
-                <option value="agua">Água</option>
-                <option value="grama">Grama</option>
-                <option value="voador">Voador</option>
-                <option value="lutador">Lutador</option>
-                <option value="veneno">Veneno</option>
-                <option value="eletrico">Elétrico</option>
-                <option value="terra">Terra</option>
-                <option value="pedra">Pedra</option>
-                <option value="psiquico">Psíquico</option>
-                <option value="gelo">Gelo</option>
-                <option value="inseto">Inseto</option>
-                <option value="fantasma">Fantasma</option>
-                <option value="ferro">Ferro</option>
-                <option value="dragao">Dragão</option>
-                <option value="sombrio">Sombrio</option>
-                <option value="fada">Fada</option>
-            </select>
-        <!--</p>
-        <p>-->
-            <label class="labelForm" for="fraqueza">Fraqueza:</label>
-            <select class="select" id="fraqueza" name="fraqueza">
-                <option value="normal">Normal</option>
-                <option value="fogo">Fogo</option>
-                <option value="agua">Água</option>
-                <option value="grama">Grama</option>
-                <option value="voador">Voador</option>
-                <option value="lutador">Lutador</option>
-                <option value="veneno">Veneno</option>
-                <option value="eletrico">Elétrico</option>
-                <option value="terra">Terra</option>
-                <option value="pedra">Pedra</option>
-                <option value="psiquico">Psíquico</option>
-                <option value="gelo">Gelo</option>
-                <option value="inseto">Inseto</option>
-                <option value="fantasma">Fantasma</option>
-                <option value="ferro">Ferro</option>
-                <option value="dragao">Dragão</option>
-                <option value="sombrio">Sombrio</option>
-                <option value="fada">Fada</option>
-            </select>
-        <!--</p>
-        <p>-->
-            <label class="labelForm" for="regiao">Região:</label>
-            <select class="select" id="regiao" name="regiao">
-                <option value="kanto">Kanto</option>
-                <option value="johto">Johto</option>
-                <option value="hoenn">Hoenn</option>
-                <option value="sinnoh">Sinnoh</option>
-                <option value="unova">Unova</option>
-                <option value="kalos">Kalos</option>
-                <option value="alola">Alola</option>
-                <option value="galar">Galar</option>
-                <option value="paldea">Paldea</option>
-            </select>
-        <!--</p>
-        <p>-->
-            <label class="labelForm" for="geracao">Geração:</label>
-            <select class="select" id="geracao" name="geracao">
-                <option value="1">Primeira</option>
-                <option value="2">Segunda</option>
-                <option value="3">Terceira</option>
-                <option value="4">Quarta</option>
-                <option value="5">Quinta</option>
-                <option value="6">Sexta</option>
-                <option value="7">Sétima</option>
-                <option value="8">Oitava</option>
-                <option value="9">Nona</option>
-            </select>
-        <!--</p>
-        <p>-->
-            <button class="buttonSalvar" type="submit">Salvar</button>
-        <!--</p>-->
-    </form>
+<div class="formBody">
+    <div class="formContainer">
+        <h1 class="title">
+            <!-- Pegá-los eu tentarei! -->
+            Salvar Pokemon
+        </h1>
+
+        <form class="form" action="/store" method="post">
+            @csrf
+            <!--<p>-->
+                <label class="labelForm" for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" value="">
+            <!--</p>
+            <p>-->
+                <label class="labelForm" for="tipo">Tipo:</label>
+                <select class="select" id="tipo" name="tipo">
+                    <option value="normal">Normal</option>
+                    <option value="fogo">Fogo</option>
+                    <option value="agua">Água</option>
+                    <option value="grama">Grama</option>
+                    <option value="voador">Voador</option>
+                    <option value="lutador">Lutador</option>
+                    <option value="veneno">Veneno</option>
+                    <option value="eletrico">Elétrico</option>
+                    <option value="terra">Terra</option>
+                    <option value="pedra">Pedra</option>
+                    <option value="psiquico">Psíquico</option>
+                    <option value="gelo">Gelo</option>
+                    <option value="inseto">Inseto</option>
+                    <option value="fantasma">Fantasma</option>
+                    <option value="ferro">Ferro</option>
+                    <option value="dragao">Dragão</option>
+                    <option value="sombrio">Sombrio</option>
+                    <option value="fada">Fada</option>
+                </select>
+            <!--</p>
+            <p>-->
+                <label class="labelForm" for="fraqueza">Fraqueza:</label>
+                <select class="select" id="fraqueza" name="fraqueza">
+                    <option value="normal">Normal</option>
+                    <option value="fogo">Fogo</option>
+                    <option value="agua">Água</option>
+                    <option value="grama">Grama</option>
+                    <option value="voador">Voador</option>
+                    <option value="lutador">Lutador</option>
+                    <option value="veneno">Veneno</option>
+                    <option value="eletrico">Elétrico</option>
+                    <option value="terra">Terra</option>
+                    <option value="pedra">Pedra</option>
+                    <option value="psiquico">Psíquico</option>
+                    <option value="gelo">Gelo</option>
+                    <option value="inseto">Inseto</option>
+                    <option value="fantasma">Fantasma</option>
+                    <option value="ferro">Ferro</option>
+                    <option value="dragao">Dragão</option>
+                    <option value="sombrio">Sombrio</option>
+                    <option value="fada">Fada</option>
+                </select>
+            <!--</p>
+            <p>-->
+                <label class="labelForm" for="regiao">Região:</label>
+                <select class="select" id="regiao" name="regiao">
+                    <option value="kanto">Kanto</option>
+                    <option value="johto">Johto</option>
+                    <option value="hoenn">Hoenn</option>
+                    <option value="sinnoh">Sinnoh</option>
+                    <option value="unova">Unova</option>
+                    <option value="kalos">Kalos</option>
+                    <option value="alola">Alola</option>
+                    <option value="galar">Galar</option>
+                    <option value="paldea">Paldea</option>
+                </select>
+            <!--</p>
+            <p>-->
+                <label class="labelForm" for="geracao">Geração:</label>
+                <select class="select" id="geracao" name="geracao">
+                    <option value="1">Primeira</option>
+                    <option value="2">Segunda</option>
+                    <option value="3">Terceira</option>
+                    <option value="4">Quarta</option>
+                    <option value="5">Quinta</option>
+                    <option value="6">Sexta</option>
+                    <option value="7">Sétima</option>
+                    <option value="8">Oitava</option>
+                    <option value="9">Nona</option>
+                </select>
+            <!--</p>
+            <p>-->
+                <button class="buttonSalvar" type="submit">Salvar</button>
+            <!--</p>-->
+        </form>
+    </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,19 +10,41 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
     <style>
+        body{
+            margin:0;
+            padding:0;
+        }
+
         * {
             font-family: "Inter", sans-serif;
+        }
+
+        .formBody{
+            width: 100%;
+            height: 100vh;
+            background-color:#c8cdc6;
         }
 
         .formContainer{
             display: grid;
             align-items: center;
             justify-content: center;
+            width: 100%;
+        }
+
+        .divTitle{
+            display: grid;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .title{
+            font-size:3rem;
         }
 
         .buttonSalvar{
             cursor: pointer;
-            width: 50%;
+            width: 60%;
             height: 5vh;
             font-size: 1rem;
             border-radius: 5px;
@@ -40,11 +62,28 @@
 
         .form{
             display:grid;
+            justify-content:center;
         }
 
         .select{
             font-size:1.2rem;
             margin-bottom:5px;
+            cursor: pointer;
+            transition: .3s;
+            padding-left: 5px;
+        }
+
+        .select:hover{
+            background-color:#1f3f42;
+            color:#c8cdc6;
+            transition: .3s;
+            appearance: none;
+            padding-left: 5px;
+
+        }
+
+        .select:active{
+            border: 0px;
         }
 
         .labelForm{
@@ -55,6 +94,13 @@
         #nome{
             margin-bottom:5px;
             font-size:1.2rem;
+            transition:.3s;
+        }
+
+        #nome:focus{
+            background-color:#1f3f42;
+            color:#c8cdc6; 
+            transition:.3s;
         }
     </style>
 </head>
